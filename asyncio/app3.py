c432faa43b7c59b7f6909a266cb0b8d37bf67c24
Sohom Chatterjee <sohom12345678910@gmail.com>
2026-01-02 11:50:51 +0530

    basics_of_asyncio_in_python

---
+++ asyncio/app3.py
@@ -0,0 +1,19 @@
+
+
+import asyncio
+import aiohttp
+
+async def fetch_url(session,url):
+    async with session.get(url) as response:
+        print(f" fetched {url}  with status {response.status}")
+
+
+
+async def main():
+    urls=[]
+    async with aiohttp.ClientSession as session:
+        tasks=[fetch_url(session,url) for url in urls]
+        await asyncio.gather(*tasks)
+
+asyncio.run(main())
+
